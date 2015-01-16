ef290c4f8a0a1180746227a5a2fda3d2ca7fbb91
ian-moran <ian@community-technology.com>
2015-01-16 12:57:53 -0500

    New OCG Theme

---
+++ docroot/sites/all/themes/new_ocg/templates/page.tpl.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+  
+  <div class="nci-banner">
+    <div class="nci-inner">
+      <a href="http://www.cancer.gov"><img src="/sites/all/themes/new_ocg/images/nci-header.JPG" alt="<?php print t('National Cancer Institute at the National Institutes of Health (www.cancer.gov)'); ?>" width="1260px"/></a>
+      <!--<img src="/sites/all/themes/ocg/images/NCI-banner.jpg" border=0 usemap="#bannermap">
+      <map name="bannermap">
+      <area shape=rect coords="13,1,257,33" href="http://www.cancer.gov" alt="National Cancer Institute">
+      <area shape=rect coords="698,10,847,24" href="http://www.nih.gov" alt="at the National Institutes of Health">
+      <area shape=rect coords="853,12,948,25" href="http://www.cancer.gov" alt="www.cancer.gov">
+      </map>-->
+    </div><!-- /NCI-inner -->
+  </div><!-- /NCI-banner -->
+  
+  <div class="dna-banner">
+  </div><!-- /dna-banner -->
+  
+  
+  <div class="outer-header">
+    <div class="disappear">
+
+  <header class="header" id="header" role="banner">
+
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    <?php endif; ?>
+
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header__name-and-slogan" id="name-and-slogan">
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name" id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($secondary_menu): ?>
+      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => $secondary_menu_heading,
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+  </header>
+    </div>
+  </div>
+<div id="page-inner">
+  <div id="main">
+    
+    <div id="pre-content">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+    <div id="content" class="column" role="main">
+      
+    </div>
+      
+    
+
+    <div id="navigation">
+
+      <?php if ($main_menu): ?>
+        <nav id="main-menu" role="navigation" tabindex="-1">
+          <?php
+          // This code snippet is hard to modify. We recommend turning off the
+          // "Main menu" on your sub-theme's settings form, deleting this PHP
+          // code block, and, instead, using the "Menu block" module.
+          // @see https://drupal.org/project/menu_block
+          print theme('links__system_main_menu', array(
+            'links' => $main_menu,
+            'attributes' => array(
+              'class' => array('links', 'inline', 'clearfix'),
+            ),
+            'heading' => array(
+              'text' => t('Main menu'),
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>
+
+      <?php print render($page['navigation']); ?>
+
+    </div>
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
+
+  </div>
+
+  <?php print render($page['footer']); ?>
+
+</div>
+</div>
+
+<?php print render($page['bottom']); ?>
