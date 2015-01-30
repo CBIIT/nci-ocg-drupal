e0974b50214fdc79c72ca04f717d6d10dadc0f93
ian-moran <ian@community-technology.com>
2015-01-30 11:07:55 -0500

    Latest changes to new theme

--- docroot/sites/all/themes/new_ocg/templates/page.tpl.php
+++ docroot/sites/all/themes/new_ocg/templates/page.tpl.php
@@ -76,6 +76,8 @@
     <div id="pre-content">
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
+    </div>
+    <div id="content" class="column" role="main">
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
@@ -91,34 +93,8 @@
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
-    <div id="content" class="column" role="main">
       
-    </div>
-      
-    
-
     <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
 
       <?php print render($page['navigation']); ?>
 
