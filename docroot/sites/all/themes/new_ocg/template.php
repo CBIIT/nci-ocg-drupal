8989459b635f4aa6652d44bd38b8bbe7cf450260
ian-moran <ian@community-technology.com>
2015-07-10 10:56:22 -0400

    Additional dpm() calls to resolve logo problem.

--- docroot/sites/all/themes/new_ocg/template.php
+++ docroot/sites/all/themes/new_ocg/template.php
@@ -121,9 +121,11 @@ function new_ocg_preprocess_page(&$variables, $hook) {
   drupal_theme_initialize();
   dpm($base_url);
   $logo = theme_get_setting('logo');
+  dpm($logo);
   if (strpos($logo, $base_url) === 0) {
     $variables['new_logo'] = drupal_substr($logo, drupal_strlen($base_url));
   }
+  dpm($variables['new_logo']);
 }
 
 function new_ocg_preprocess_node(&$variables) {
