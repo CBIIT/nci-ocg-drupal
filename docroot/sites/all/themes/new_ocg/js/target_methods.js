bd93e851cebc25f21b183780175562caf3194a64
ian-moran <ian@community-technology.com>
2015-12-04 12:22:55 -0500

    TARGET Methods javascript fix.

--- docroot/sites/all/themes/new_ocg/js/target_methods.js
+++ docroot/sites/all/themes/new_ocg/js/target_methods.js
@@ -1,15 +1,15 @@
 (function($) {
   Drupal.behaviors.targetMethods = {
     attach: function(context, settings) {
-      $('.view-display-id-description span').hide();
-      $('.view-display-id-projects_description span').hide();
+      $('.view-display-id-description span:first-child').hide();
+      $('.view-display-id-projects_description span:first-child').hide();
 
       $(".views-field-field-sample-preparation-protoco a").click(function(e) {
         e.preventDefault();
         var $this = $(this);
         history.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
-        $('.view-display-id-description span').hide();
+        $('.view-display-id-description span:first-child').hide();
         $(url).show();
       });
       $(".views-field-field-data-analysis-protocols a").click(function(e) {
@@ -17,7 +17,7 @@
         var $this = $(this);
         history.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
-        $('.view-display-id-projects_description span').hide();
+        $('.view-display-id-projects_description span:first-child').hide();
         $(url).show();
       });
     }
