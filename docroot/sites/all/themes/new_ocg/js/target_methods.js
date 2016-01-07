569c6846592c385c2dc4f53847a210cb4d3c7ac2
ian-moran <ian@community-technology.com>
2016-01-07 11:05:12 -0500

    OCG-105 fix to ensure that descriptions do not leave space above them.

--- docroot/sites/all/themes/new_ocg/js/target_methods.js
+++ docroot/sites/all/themes/new_ocg/js/target_methods.js
@@ -1,9 +1,9 @@
 (function($) {
   Drupal.behaviors.targetMethods = {
     attach: function(context, settings) {
-      $('.view-display-id-description .field-item span').show();
+      $('.view-display-id-description .views-row').show();
       $('.view-display-id-projects_description .field-item span').show();
-      $('.view-display-id-description .hide-description').hide();
+      $('.view-display-id-description .views-row').hide();
       $('.view-display-id-projects_description .hide-description').hide();
 
       $(".views-field-field-sample-preparation-protoco a").click(function(e) {
@@ -11,8 +11,8 @@
         var $this = $(this);
         history.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
-        $('.view-display-id-description .hide-description').hide();
-        $('.view-display-id-description .field-item span').show();
+        $('.view-display-id-description .views-row').hide();
+        $(url).parent().parent().parent().show();
         $(url).show();
       });
       $(".views-field-field-data-analysis-protocols a").click(function(e) {
