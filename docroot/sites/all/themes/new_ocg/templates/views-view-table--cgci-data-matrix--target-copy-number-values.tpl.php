c117a6a894035631ba2caf3da027a5a0ca2bb585
ian-moran <ian@community-technology.com>
2016-05-12 15:51:34 -0400

    TARGET small screen templates

---
+++ docroot/sites/all/themes/new_ocg/templates/views-view-table--cgci-data-matrix--target-copy-number-values.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+
+    <?php foreach ($rows as $row_count => $row): ?>
+      
+            <?php if ($row['field_availability'] == 'Unavailable'): ?>
+                  <span class='patientcellelem'><?php print $row['field_link']; ?></span>
+                  <br />
+                <?php endif; ?>
+                <?php if ($row['field_availability'] == 'Protected'): ?>
+                  <a class="protected patientcellelem" target="_blank" href='<?php print $row['field_link_1']; ?>'><?php print $row['field_link'] . '†'; ?></a>
+                  <br />
+                <?php endif; ?>
+                <?php if ($row['field_availability'] == 'Public'): ?>
+                  <a class="public patientcellelem" target="_blank" href=''<?php print $row['field_link_1']; ?>'><?php print $row['field_link'] . '*'; ?></a>
+                  <br />
+                <?php endif; ?>
+                <?php print $row['view']; ?>
+          
+    <?php endforeach; ?>
+ 
