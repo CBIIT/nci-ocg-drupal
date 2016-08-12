1144fe745227eafe2e4475615bb95453b646c75a
ian-moran <ian@community-technology.com>
2016-08-12 12:19:34 -0400

    Added history.js for ie use of replaceState.

--- docroot/sites/all/themes/new_ocg/js/target_methods.js
+++ docroot/sites/all/themes/new_ocg/js/target_methods.js
@@ -9,7 +9,7 @@
       $(".view-display-id-table .views-field-field-sample-preparation-protoco a").click(function(e) {
         e.preventDefault();
         var $this = $(this);
-        history.pushState(null, '', $this.attr('href'));
+        History.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
         $('.view-display-id-description .views-row').hide();
         $(url).parent().parent().parent().show();
@@ -18,7 +18,7 @@
       $(".view-display-id-projects_table .views-field-field-data-analysis-protocols a").click(function(e) {
         e.preventDefault();
         var $this = $(this);
-        history.pushState(null, '', $this.attr('href'));
+        History.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
         $('.view-display-id-projects_description .hide-description').hide();
         $('.view-display-id-projects_description .field-item span').show();
@@ -27,7 +27,7 @@
       $(".view-display-id-projects_table .views-field-field-column-three a").click(function(e) {
         e.preventDefault();
         var $this = $(this);
-        history.pushState(null, '', $this.attr('href'));
+        History.replaceState(null, '', $this.attr('href'));
         var url = window.location.hash;
         $('.view-display-id-projects_description .hide-description').hide();
         $('.view-display-id-projects_description .field-item span').show();
