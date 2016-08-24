bfd6584ce5dd56bca2a88286714db03cfecedd0f
ian-moran <ian@community-technology.com>
2016-08-24 11:54:30 -0400

    Sanitized link in analytical tools to prevent XSS.

--- docroot/sites/all/themes/new_ocg/templates/views-view-summary-unformatted--analytical_tools--attachment.tpl.php
+++ docroot/sites/all/themes/new_ocg/templates/views-view-summary-unformatted--analytical_tools--attachment.tpl.php
@@ -41,8 +41,8 @@ $total = array();
     } else {
       $items_per_page = '';
     }
-
-    $row->url = '/' . current_path() . '?' . $tool_type . $items_per_page . $page_number . '#' . $row->link;
+    $new_url = filter_xss(current_path());
+    $row->url = '/' . $new_url . '?' . $tool_type . $items_per_page . $page_number . '#' . $row->link;
     ?>
 
     <?php print (!empty($options['inline']) ? '<span' : '<div') . ' class="views-summary views-summary-unformatted">'; ?>
