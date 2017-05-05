fc6a00c92e6e1e9735c83b69f26853383d5f9ad6
ian-moran <ian@community-technology.com>
2017-05-05 13:17:42 -0400

    CTD2 Accordion templates.

---
+++ sites/all/themes/new_ocg/templates/views-view-accordion--ctd2-data-portal--accordion.tpl.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file
+ * Displays the items of the accordion.
+ *
+ * @ingroup views_templates
+ *
+ * Note that the accordion NEEDS <?php print $row ?> to be wrapped by an
+ * element, or it will hide all fields on all rows under the first field.
+ *
+ * Also, if you use field grouping and use the headers of the groups as the
+ * accordion headers, these NEED to be inside h3 tags exactly as below
+ * (though you can add classes).
+ *
+ * The current div wraping each row gets two css classes, which should be
+ * enough for most cases:
+ *     "views-accordion-item"
+ *      and a unique per row class like item-0
+ */
+
+?>
+<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?> width="100%">
+   <?php if (!empty($title) || !empty($caption)) : ?>
+     <caption><?php print $caption . $title; ?></caption>
+  <?php endif; ?>
+    <thead>
+      <tr>
+        
+        <th id="projtitle_ch" class="colheader" scope="col" width="46%">Project Title</th>
+        <th id="expapp_ch" class="colheader" scope="col" width="14%">Experimental<br>Approaches</th>
+        <th id="data_ch" class="colheader" scope="col" width="9%">Data</th>
+        <th id="pi_ch" class="colheader" scope="col"width="13%">Principal<br>Investigator</th>
+        <th id="contact_ch" class="colheader" scope="col" width="10%">Contact Name</th>
+        <th id="contact_ch" class="colheader" scope="col" width="8%">Submission Date</th>
+      </tr>
+    </thead>
+</table>
+<?php if (!empty($title)): ?>
+  <h3 class="<?php print $view_accordion_id; ?>">
+    <?php print $title; ?>
+  </h3>
+<?php endif; ?>
+<?php if ($use_group_header): ?><div><?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div class="<?php print $classes_array[$id]; ?>">
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
+<?php if ($use_group_header): ?></div><?php endif; ?>
---
+++ sites/all/themes/new_ocg/templates/views-view-table--ctd2-data-portal--project-title-accordion.tpl.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+<table>
+    <?php foreach ($rows as $row_count => $row): ?>
+      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+        <?php foreach ($row as $field => $content): ?>
+          <th width="46%" id="<?php print $result[$row_count]->field_collection_item_field_data_field_row_item_id . '_rh'; ?>" scope="row"<?php if ($field_classes[$field][$row_count]) { print 'class="rowheader '. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <div class="projcell">
+            <?php print $content; ?>
+            </div>
+          </th>
+        
+          <?php print views_embed_view('ctd2_data_portal', 'experimental_approaches', $result[$row_count]->field_collection_item_field_data_field_row_item_id); ?>
+          <td width="9%" class="outer colN" headers="data_ch <?php print $result[$row_count]->nid . '_bn ' . $result[$row_count]->field_collection_item_field_data_field_row_item_id . '_rh'; ?>"><?php print views_embed_view('ctd2_data_portal', 'data', $result[$row_count]->field_collection_item_field_data_field_row_item_id); ?></td>
+          <?php 
+          $view_result = views_get_view_result('ctd2_data_portal', 'principal_investigator', $result[$row_count]->field_collection_item_field_data_field_row_item_id);
+          if(!empty($view_result[0]->field_field_pi_link)){ ?>
+          <?php print views_embed_view('ctd2_data_portal', 'principal_investigator', $result[$row_count]->field_collection_item_field_data_field_row_item_id); ?>
+          <?php } ?>
+          <?php print views_embed_view('ctd2_data_portal', 'contact_name', $result[$row_count]->field_collection_item_field_data_field_row_item_id); ?>
+          <?php print views_embed_view('ctd2_data_portal', 'submission_date', $result[$row_count]->field_collection_item_field_data_field_row_item_id); ?>
+      <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+</table>
