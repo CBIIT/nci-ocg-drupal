2feb3b3197adb227cafb85d4b63d2ac6d993177d
ian-moran <ian@community-technology.com>
2018-06-12 14:44:22 -0400

    Latest changes for OCG Json functionality.

--- sites/all/modules/custom/ocg_json/js/ocg_json.gen.js
+++ sites/all/modules/custom/ocg_json/js/ocg_json.gen.js
@@ -1,59 +1,190 @@
-var app = angular.module('app', ['ngSanitize']).controller('datacontroller', function($scope, $http){
-  $http.get('/programs/ctd2/data-portal-json').success(function(result){
+var app = angular.module('app', ['ngSanitize']).controller('datacontroller', function ($scope, $http) {
+  $http.get('/programs/ctd2/data-portal-json').success(function (result) {
     $scope.ctd2nodes = result;
+    const assayList = [];
+    angular.forEach($scope.ctd2nodes.nodes, function (nodes, key) {
+      angular.forEach(nodes.node.row, function (row, key) {
+        angular.forEach(row.assay_type, function (assay_type, key) {
+          assayList.push({assay: assay_type.name});
+        });
+      });
+    });
+
+    assays = assayList.reduce(function (tally, method) {
+      tally[method.assay] = (tally[method.assay] || 0) + 1;
+      return tally;
+    }, {});
+
+    var assaysObj = [];
+    angular.forEach(assays, function (number, key) {
+      assaysObj.push({assay: key, number: number, node: {}});
+    });
+
+    assaysObj = assaysObj.sort((a, b) => a.assay.localeCompare(b.assay));
+
+    angular.forEach(assaysObj, function (assays, assayObjKey) {
+      var count = 0;
+      angular.forEach($scope.ctd2nodes.nodes, function (nodes, nodesKey) {
+        angular.forEach(nodes.node.row, function (row, rowKey) {
+          angular.forEach(row.assay_type, function (assay_type, assayKey) {
+            if (assays.assay == assay_type.name) {
+              var dppCount = 0;
+              var dataCount = 0;
+              var investigatorCount = 0;
+              var contactCount = 0;
+              var paperCount = 0;
+              assaysObj[assayObjKey].node[count] = {
+                id: nodes.node.id,
+                row_number: row.row_number,
+                project_title: row.project_title.title,
+                project_title_url: row.project_title.url,
+                dpp_title: row.dpp.title,
+                dpp_body: row.dpp.body,
+                dpp: {},
+                data: {},
+                investigator: {},
+                contact: {},
+                submission_date: row.submission_date,
+                paper: {}
+              };
+              if (typeof row.dpp[0] !== 'undefined') {
+                angular.forEach(row.dpp, function (dppRow, dppKey) {
+                  assaysObj[assayObjKey].node[count].dpp[dppCount] = {dpp_title: dppRow.dpp_title, dpp_body: dppRow.dpp_body};
+                  dppCount++
+                });
+              }
+              ;
+              if (typeof row.data[0] !== 'undefined') {
+                angular.forEach(row.data, function (dataRow, dataKey) {
+                  if (dataRow.data_link !== null) {
+                    assaysObj[assayObjKey].node[count].data[dataCount] = {data_title: dataRow.data_link.title, data_url: dataRow.data_link.url};
+                    dataCount++;
+                  }
+                  ;
+                });
+              }
+              ;
+              if (typeof row.investigator !== 'undefined') {
+                if (typeof row.investigator[0] !== 'undefined') {
+                  angular.forEach(row.investigator, function (investigatorRow, investigatorKey) {
+                    if (investigatorRow.investigator !== null) {
+                      assaysObj[assayObjKey].node[count].investigator[investigatorCount] = {investigator: investigatorRow.investigator};
+                      investigatorCount++;
+                    }
+                    ;
+                  });
+                }
+                ;
+              }
+              ;
+              if (typeof row.contact !== 'undefined') {
+                if (typeof row.contact[0] !== 'undefined') {
+                  angular.forEach(row.contact, function (contactRow, contactKey) {
+                    assaysObj[assayObjKey].node[count].contact[contactCount] = contactRow;
+                    //angular.forEach(contactRow, function (contactLinkRow, contactLinkKey) {
+                      //if (contactLinkRow.title !== null) {
+                        //assaysObj[assayObjKey].node[count].contact[contactCount] = {contact_title: contactLinkRow.title, contact_url: contactLinkRow.url};
+                        //contactCount++;
+                      //};
+                      //console.log(contactLinkRow);
+                    //});
+                  });
+                }
+                ;
+              }
+              ;
+              if (typeof row.paper !== 'undefined') {
+                if (typeof row.paper[0] !== 'undefined') {
+                  angular.forEach(row.paper, function (paperRow, paperKey) {
+                    if (paperRow.paper_link !== null) {
+                      assaysObj[assayObjKey].node[count].paper[paperCount] = {paper_title: paperRow.paper_link.title, paper_url: paperRow.paper_link.url};
+                      paperCount++;
+                    }
+                    ;
+                  });
+                }
+                ;
+              }
+              ;
+              count++;
+            }
+            ;
+          });
+        });
+      });
+    })
+    $scope.methods = assaysObj;
+    console.log(assaysObj);
   });
-  $scope.filterKey = function(filter) {
-    //console.log(row);
-    if(row.row_number == filter.row_number)
-    {
-      console.log(row.row_number);  
-      return true; // this will be listed in the results
-    }
-    return false; // otherwise it won't be within the results
-  };
+
+  $scope.clickChoice = 'centers';
+  $scope.revealData = function (value) {
+    if (value == 'centers')
+      return true;
+    else
+      return false;
+  }
   $scope.filterRow = '';
-  $scope.setRow = function(row) {
-    console.log(row);
+  $scope.filterId = '';
+  $scope.setRow = function (row) {
+    $scope.filterRow = row;
+  };
+
+  $scope.setMethod = function (id, row) {
+    $scope.filterId = id;
     $scope.filterRow = row;
   }
   
-}).filter('sameRowNumber', function(){
-  return function(values, rowNumber) {
-    if(!rowNumber) {
+  $scope.filterMethodRow = function(items) {
+    var result = {};
+    if ($scope.filterRow && $scope.filterId) {
+      angular.forEach(items, function(value, key) {
+        if (value.id  === $scope.filterId && value.row_number === $scope.filterRow) {
+          result[key] = value;
+        };
+      });
+    } else {
+      result = items;
+    };
+    return result;
+  };
+}).filter('sameRowNumber', function () {
+  return function (values, rowNumber) {
+    if (!rowNumber) {
       // initially don't filter
       return values;
     }
     // filter when we have a selected groupId
-    return values.filter(function(value){
+    return values.filter(function (value) {
       return value.row_number === rowNumber;
     })
   }
-}).filter('crop', function(){
-	return function(input, limit, respectWordBoundaries, suffix){
-		if (input === null || input === undefined || limit === null || limit === undefined || limit === '') {
-			return input;
-		}
-		if (angular.isUndefined(respectWordBoundaries)) {
-			respectWordBoundaries = true;
-		}
-		if (angular.isUndefined(suffix)) {
-			suffix = ' ';
-		}
+}).filter('crop', function () {
+  return function (input, limit, respectWordBoundaries, suffix) {
+    if (input === null || input === undefined || limit === null || limit === undefined || limit === '') {
+      return input;
+    }
+    if (angular.isUndefined(respectWordBoundaries)) {
+      respectWordBoundaries = true;
+    }
+    if (angular.isUndefined(suffix)) {
+      suffix = ' ';
+    }
 
-		if (input.length <= limit) {
-			return input;
-		}
+    if (input.length <= limit) {
+      return input;
+    }
 
-		limit = limit - suffix.length;
+    limit = limit - suffix.length;
 
-		var trimmedString = input.substr(0, limit);
-		if (respectWordBoundaries) {
-			return trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + suffix;
-		}
-		return trimmedString + suffix;
-}
+    var trimmedString = input.substr(0, limit);
+    if (respectWordBoundaries) {
+      return trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + suffix;
+    }
+    return trimmedString + suffix;
+  }
 });
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
   angular.bootstrap(document.getElementById('data-portal-app'), ['app']);
 });
--- sites/all/themes/new_ocg/templates/ocg_json.tpl.php
+++ sites/all/themes/new_ocg/templates/ocg_json.tpl.php
@@ -2,10 +2,11 @@
 
   <div class="portal-navigation">
     <div id="centers">
-      <div class="centers-inner">{{ctd2nodes.nodes.length}} Centers</div>
+      <a href ng-click="clickChoice = 'centers'; setRow(); projectTitle = ctd2nodes.nodes[0].node.title.title" class="centers-inner">{{ctd2nodes.nodes.length}} Centers</a>
+      <a href ng-click="clickChoice = 'method'; setMethod(methods[0].node[0].id, method.node[0].row_number); methodTitle = methods[0]">{{methods.length}} Methods</a>
     </div>
 
-    <div id="center-title">
+    <div id="center-title" ng-show="revealData(clickChoice)">
       <div class="center-title-inner">
         <div ng-repeat="project in ctd2nodes.nodes" class="project">
           <div class="title"><a ng-click="$parent.projectTitle = project.node.title.title; setRow(project.node.row[0].row_number)">{{ project.node.title.title}}</a></div>
@@ -13,8 +14,17 @@
         </div>
       </div>
     </div>
+    
+    <div id="method-title" ng-show="!revealData(clickChoice)">
+      <div class="method-title-inner">
+        <div ng-repeat="method in methods" class="project">
+          <div class="title"><a ng-click="$parent.methodTitle = method">{{method.assay}}</a></div>
+          <div class="dataset-count">{{method.number}} Datasets</div>
+        </div>
+      </div>
+    </div>
 
-    <div id="project-title">
+    <div id="project-title" ng-show="revealData(clickChoice)">
       <div class="project-title-inner">
         <div ng-repeat="project in ctd2nodes.nodes| filter:projectTitle | limitTo:1">
           <div ng-repeat="row in project.node.row">
@@ -23,17 +33,27 @@
         </div>
       </div>
     </div>
+  
+    <div id="method-project-title" ng-show="!revealData(clickChoice)">
+      <div class="project-title-inner">
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="row in method.node | limitTo:1">
+            <a ng-click="setMethod(row.id, row.row_number)">{{row.project_title}}</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <div id="row-info">
+  <div id="row-info" ng-show="revealData(clickChoice)">
     <div class="row-info-approaches">
       <div id="experimental-approaches">
 
         <div ng-repeat="project in ctd2nodes.nodes| filter:projectTitle | limitTo:1">
-          <div ng-repeat="row in project.node.row| sameRowNumber:filterRow | limitTo:1">
+          <div ng-repeat="row in project.node.row | sameRowNumber:filterRow | limitTo:1">
             <h2>{{row.dpp.title}}</h2>
-            <div class="appraoches-description">
-            <div ng-bind-html="row.dpp.body | crop: 200"></div><a href="{{row.project_title.url}}"> {{row.dpp.body.length > 200 ? 'Read More...' : ''}}</a>
+            <div class="approaches-description">
+              <div ng-bind-html="row.dpp.body | crop: 200"></div><a href="{{row.project_title.url}}"> {{row.dpp.body.length > 200 ? 'Read More...' : ''}}</a>
             </div>
             <h3 class="approaches-header">Experimental Approaches</h3>
             <div ng-repeat="approach in row.dpp" class="approaches-listing">
@@ -96,6 +116,84 @@
           <div ng-repeat="row in project.node.row| sameRowNumber:filterRow">
             <div ng-repeat="paper in row.paper">
               <a href="/{{paper.paper_link.url}}">{{paper.paper_link.title}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="method-row-info" ng-show="!revealData(clickChoice)">
+    <div class="row-info-approaches">
+      <div id="experimental-approaches">
+
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <h2>{{row.dpp_title}}</h2>
+            <div class="approaches-description">
+              <div ng-bind-html="row.dpp_body | crop: 200"></div><a href="{{row.project_title_url}}"> {{row.dpp_body.length > 200 ? 'Read More...' : ''}}</a>
+            </div>
+            <h3 class="approaches-header">Experimental Approaches</h3>
+            <div ng-repeat="approach in row.dpp" class="approaches-listing">
+              <div class="approaches-listing-title">{{approach.dpp_title}}</div>
+              <div ng-bind-html="approach.dpp_body | crop: 150"></div><a href="{{row.project_title_url}}"> {{approach.dpp_body.length > 150 ? 'Read More...' : ''}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row-info-remaining">
+      <div id="data">
+        <h3 class="data-header">Data</h3>
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <div ng-repeat="data in row.data">
+              Access the <a href="/{{data.data_url}}">{{data.data_title}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="investigator">
+        <h3 class="investigator-header">Principal Investigator</h3>
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <div ng-repeat="investigator in row.investigator">
+              <div>{{investigator.investigator}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="contact">
+        <h3 class="contact-header">Contact Name</h3>
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <div ng-repeat="contact in row.contact">
+              <div ng-repeat="contact_link in contact.contact_link">
+                <a href="/{{contact_link.url}}">{{contact_link.title}}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="submission-date">
+        <h3 class="submission-date-header">Submission Date</h3>
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <div>{{row.submission_date * 1000| date : "y-MM-dd"}}</div>
+          </div>
+        </div>
+      </div>
+
+      <div id="reference">
+        <h3 class="reference-header">Reference</h3>
+        <div ng-repeat="method in methods | filter:methodTitle | limitTo:1">
+          <div ng-repeat="(key, row) in filterMethodRow(method.node)" ng-if="$index < 1">
+            <div ng-repeat="paper in row.paper">
+              <a href="/{{paper.paper_url}}">{{paper.paper_title}}</a>
             </div>
           </div>
         </div>
