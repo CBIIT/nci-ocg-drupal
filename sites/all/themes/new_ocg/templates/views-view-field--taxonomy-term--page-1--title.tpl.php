bc1873435eae4ef4d3a27ec4c3ccbcb36957b2d0
ian-moran <ian@community-technology.com>
2019-04-30 15:06:08 -0400

    Changing the url's for addThis sharing.

--- sites/all/themes/new_ocg/templates/views-view-field--taxonomy-term--page-1--title.tpl.php
+++ sites/all/themes/new_ocg/templates/views-view-field--taxonomy-term--page-1--title.tpl.php
@@ -27,8 +27,8 @@ $alias = $base_root . '/' . drupal_get_path_alias('node/'.$row->nid);
 <a name="<?php print $row->nid; ?>"></a>
 <span><?php print $row->field_field_enews_category[0]['rendered']['#label']; ?></span>
 <span class="share-buttons">
-  <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-    <a class="addthis_button_facebook" addthis:url="<?php print $alias ?>?cid=fb_ocg-updates_sharedposts" addthis:title="<?php print $row->node_title; ?>"></a>
+  <div class="addthis_toolbox addthis_default_style addthis_32x32_style" addthis:title="<?php print $row->node_title; ?>">
+    <a class="addthis_button_facebook" addthis:url="<?php print $alias ?>?cid=fb_ocg-updates_sharedposts"></a>
     <a class="addthis_button_twitter" addthis:url="<?php print $alias ?>?cid=tw_ocg-updates_sharedposts"></a>
     <a class="addthis_button_linkedin" addthis:url="<?php print $alias ?>?cid=li_ocg-updates_sharedposts"></a>
     <a class="addthis_button_email" addthis:url="<?php print $alias ?>?cid=em_ocg-updates_sharedposts"></a>
